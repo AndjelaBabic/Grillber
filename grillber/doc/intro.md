c89e7b2339e549f0b5c36e4b1e8c39eef0bb0348
Andjela Babic <andjelasbabic@gmail.com>
2019-09-14 16:47:16 +0200

    Project setup

---
+++ grillber/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to grillber
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ grillber/test/grillber/core_test.clj
@@ -0,0 +1,7 @@
+(ns grillber.core-test
+  (:require [clojure.test :refer :all]
+            [grillber.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

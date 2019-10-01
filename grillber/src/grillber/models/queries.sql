3221f6fbe3bdcd7adc747c95447751c188cc06f1
Andjela Babic <andjela.babic@vanderlande.com>
2019-10-01 15:56:52 +0200

    Added queries in queries.sql

--- grillber/src/grillber/models/queries.sql
+++ grillber/src/grillber/models/queries.sql
@@ -3,10 +3,10 @@
 INSERT INTO user (first_name, last_name, username, email, password)
 VALUES (:first_name, :last_name, :username, :email, :password);
 
--- name: get-user-by-username
+-- name: get-user-by-username-and-password
 -- retrives a user with the given username
 SELECT * FROM user
-WHERE username = :username;
+WHERE username = :username AND password = :password;
 
 -- name: get-user-by-id
 -- retrives a user with the given id
@@ -17,3 +17,25 @@ WHERE id = :id;
 -- inserting order
 INSERT INTO orders (userid, grillid, delivery_time, pickup_time, addressid, status)
     VALUES (:userid, :grillid, :delivery_time, :pickup_time, :addressid, :status);
+    
+-- name: get-all-orders
+-- retrive orders
+SELECT *
+FROM orders;
+
+-- name: get-all-orders-by-user-id
+-- retrive orders for specific user
+SELECT *
+FROM orders
+WHERE userid = :userid;
+
+-- name: update-order!
+-- updates order
+UPDATE orders
+SET grillid = :grillid
+WHERE id = :id;
+
+-- name: delete-order-by-id!
+-- deletes from orders by id
+DELETE FROM orders
+  WHERE id = :id;
